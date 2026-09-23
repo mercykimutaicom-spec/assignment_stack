cf26954b69e5efd1404153361f492abb68d8953c
Mercy Kimutai <mercykimutai.com@gmail.com>
2026-09-23 15:11:18 +0300

    Add files via upload

---
+++ algorithm.cpp
@@ -0,0 +1,12 @@
+//Question 1:1!=1
+//Algorithm
+#include <iostream>
+#include <vector>
+
+
+//Description of Question:Developed a code to solve the problem using stack
+
+//Implementation of n!
+
+
+//Ques
